09850c7ce6f6a5ad7dbc90ec70fab812e9776f84
jmpfbmx <jmpf.bmx@gmail.com>
2023-06-11 11:23:01 +0200

    lena: Initial vendor blobs
     * Extracted from: Sony/pdx213/pdx213:11/RKQ1.220715.001/1:user/release-keys
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I28a63ca0c276046e4bd2363f8d8a2cb8df9ad91b

---
+++ proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
@@ -0,0 +1,61 @@
+/*
+  Copyright (c) 2020 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+*/
+
+/* SONY BEGIN */
+/* JIML-299432 - According to ORM, local ecc shall be set up as normal call */
+/*
+BEGIN TRANSACTION;
+
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 7);
+
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','15','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','17','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','18','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','115','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','119','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','191','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','196','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','197','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','116000','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','21','116117','','');
+
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','15','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','17','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','18','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','115','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','119','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','191','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','196','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','197','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','116000','','');
+INSERT INTO qcril_emergency_source_mcc_mnc_table VALUES('208','88','116117','','');
+
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','15','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','17','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','18','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','115','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','119','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','191','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','196','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','197','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','116000','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','21','116117','','');
+
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','15','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','17','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','18','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','115','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','119','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','191','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','196','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','197','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','116000','','');
+INSERT INTO qcril_emergency_source_voice_mcc_mnc_table VALUES('208','88','116117','','');
+
+COMMIT TRANSACTION;
+
+*/
+/* SONY END */
